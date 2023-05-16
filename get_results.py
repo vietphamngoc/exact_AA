2bfe294b968d6f5a85db0173d6fe0f13d4098215
vietphamngoc <viphamngoc@gmail.com>
2023-05-16 14:23:56 +0100

    added n=5 and 7

--- get_results.py
+++ get_results.py
@@ -1,7 +1,8 @@
 from code.stats.get_stats import run_stats
 
-for n in [4,6,8]:
-	for k_0 in [0,1,2,3,4]:
+for n in range(9):
+	run_stats(n, 16, 50, False)
+	for k_0 in range(5):
 		run_stats(n, 16, 50, k_0=k_0, step=2)
 
 
