4ef6a24b9e18f29369fc9196a7193227627f0ede
vietphamngoc <viphamngoc@gmail.com>
2023-06-22 09:25:19 +0100

    Changed upd strat

--- code/update_strat/updates.py
+++ code/update_strat/updates.py
@@ -3,7 +3,7 @@ import code.utilities.utility as util
 from code.circuits.tnn import TNN
 
 
-def add_remove(ones: set, to_update: list, active: list):
+def add_remove(ones: set, to_update: list, active: list, threshold: int):
     to_update.append(ones)
     for a_g in active:
         if (ones.issubset(a_g)) and (a_g not in to_update):
@@ -30,30 +30,26 @@ def update_delta(n: int, measurements: dict, tun_net: TNN):
 
 def update_junta(n: int, k: int, measurements: dict, tun_net: TNN):
     active = [util.str_to_ones(o) for o,v in tun_net.gates.items() if v==1]
+    print(f"active: {active}")
+    to_update = []
+    ordered_errors = [[] for _ in range(n+1)]
 
-    if "0"*n in measurements['errors']:
-        to_update = [set()]+active
+    for err in measurements['errors']:
+        ones = util.str_to_ones(err)
+        ordered_errors[len(ones)].append(ones)
 
-    else:
-        to_update = []
-        ordered_errors = [[] for _ in range(n+1)]
-
-        for err in measurements['errors']:
-            ones = util.str_to_ones(err)
-            ordered_errors[len(ones)].append(ones)
-
-        for i in range(k+1):
-            for ones in ordered_errors[i]:
-                add_remove(ones, to_update, active)
-
-        for i in range(k+1, n+1):
-            for ones in ordered_errors[i]:
-                subset = False
-                for upd in to_update:
-                    if upd.issubset(ones):
-                        subset = True
-                        break
-                if not subset:
-                    add_remove(ones, to_update, active) 
-
+    # for i in range(k+1):
+    #     for ones in ordered_errors[i]:
+    #         add_remove(ones, to_update, active)
+    # print(f"upd1: {to_update}")
+    for i in range(n+1):
+        for ones in ordered_errors[i]:
+            subset = False
+            for upd in to_update:
+                if upd.issubset(ones):
+                    subset = True
+                    break
+            if not subset:
+                add_remove(ones, to_update, active, k) 
+    # print(f"upd2: {to_update}")
     return [util.ones_to_str(o, n) for o in to_update]
